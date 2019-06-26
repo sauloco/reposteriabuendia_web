30a9f15e79394e3a22465f585cc30b8e2f7f8ca8
Florencia Soncini <florenciasoncini@gmail.com>
2019-06-26 18:20:07 -0300

    agregamos menu borramos script agregamos titulo

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-window.addEventListener('load', onLoad);
 
-//Hola Pame bbcita mi amorrrrrrr... florchu te lovea fuerte<3
-//TE AMOO BEBEEEEE
-
-function onLoad() {
-  const quienLoAbre = new URL(location.href).searchParams.get('name');
-  document.querySelector('h2').innerHTML = `Bienvenido: ${quienLoAbre}`;
-}
 
